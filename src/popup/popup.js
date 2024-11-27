7de5cad3cebb6ad487d98da100799f0460ff78bf
JesusRdzT <jesusrdzt00@gmail.com>
2024-11-27 12:15:12 -0600

    Feat: Unstyled Main/Translator/Storyteller/Prompt UX

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -3,3 +3,35 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   code.innerText = JSON.stringify(message, null, 2);
   document.body.appendChild(code);
 });
+
+function setupPromptSection() {
+  const sessions = [] // TODO: Get from storage
+
+  const promptSection = document.getElementById("prompt-section");
+  promptSection.innerHTML = "";
+
+  if (sessions.length > 0) {
+    const ul = document.createElement("ul");
+    ul.id = "saved-sessiones-list";
+
+    sessions.forEach((session, index) => {
+      const li = document.createElement("li");
+      li.innerText = session.name;
+      li.addEventListener("click", () => {
+      });
+      ul.appendChild(li);
+    });
+
+    promptSection.appendChild(ul);
+
+  } else {
+    const p = document.createElement("p");
+    p.innerText = "No sessions found";
+    promptSection.appendChild(p);
+  }
+}
+
+window.addEventListener("load", () => {
+  setupPromptSection();
+
+});
---
+++ src/popup/translator.js
@@ -0,0 +1,10 @@
+const LANGUGAGES = [
+  { code: "en", name: "English" },
+  { code: "es", name: "Spanish" },
+  { code: "fr", name: "French" }
+];
+
+
+window.addEventListener("load", () => {
+  
+});
