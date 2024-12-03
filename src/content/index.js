6af48acc334ff1349c17149a03a1d56aa6ae024d
JesusRdzT <jesusrdzt00@gmail.com>
2024-12-02 18:36:42 -0600

    Update: Removing unused files

--- src/content/index.js
+++
@@ -1,7 +0,0 @@
-import { showAssistantDialog } from './modal-dialog.js';
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "showContextDialog") {
-    showAssistantDialog(message.context);
-  }
-});
--- src/content/main-popup.js
+++
@@ -1,9 +0,0 @@
-// import { createMainDialog } from './extension-components.js';
-
-export function startMainExperience() {
-	/*
-	const { node: mainDialog, ...mainBuilder } = createMainDialog();
-	console.log("shows the dialog");
-	mainDialog.show();
-	*/
-}
